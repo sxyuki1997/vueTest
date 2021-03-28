c31bb3975b64faa57855690f3886ad1c394b8ca1
sxyuki1997 <yishenxinyu@163.com>
2021-03-28 22:46:42 +0800

    router修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
     <ul>
       <li>
         <a
@@ -88,7 +86,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      // msg: 'Welcome to Your Vue.js App'
     }
   }
 }
